4cad4834c065f03fce591a08b3c09138ad0b40b7
Brunhilde <brandontu35@gmail.com>
2025-09-09 20:46:26 -0400

    feat styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -25,14 +25,14 @@ function App() {
   return (
     <>
 
-      <h1>Babies first chatbot</h1>
+      <h1 className='fixed top-0 left-0 right-0 mx-auto w-fit '>Babies' First Chatbot</h1>
       <div id='chat'>
         {texts}
       </div>
-      <form action={callMe}>
+      <form action={callMe} className='fixed bottom-0 left-0 right-0 mx-auto w-fit'>
         <label htmlFor="chat" className="sr-only">Your message</label>
         <div className="flex items-center px-3 py-2 m-2 rounded-lg bg-gray-50 dark:bg-gray-700 ">
-          <textarea name="chat" rows={1} className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
+          <textarea name="chat" rows={1} className="block mx-4 p-2.5 w-lg text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ask your query..."></textarea>
           <button type="submit" className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
             <svg className="w-5 h-5 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
               <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
--- client/src/components/message.tsx
+++ client/src/components/message.tsx
@@ -15,8 +15,8 @@ export default function TextMsg(props: PropsWithChildren<Props>) {
                     <span className="text-sm font-semibold text-gray-900 dark:text-white">{props.name}</span>
                     <span className="text-sm font-normal text-gray-500 dark:text-gray-400">{props.timestamp}</span>
                 </div>
-                <div className="flex flex-col leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
-                    <p className="text-sm font-normal text-gray-900 dark:text-white">{props.text}</p>
+                <div className="flex flex-col leading-1.5 p-4 border-gray-200 bg-orange-100 rounded-e-xl rounded-es-xl">
+                    <p className="text-sm font-normal text-stone-900">{props.text}</p>
                 </div>
             </div>
         </div>
